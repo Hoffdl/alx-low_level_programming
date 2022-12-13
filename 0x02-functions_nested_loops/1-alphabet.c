01374618209aa9e4dbb352b6a0e0054b716a8216
Ikegwuonu Henry <ikegwuonuhenry25@gmail.com>
2022-12-13 15:51:54 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
