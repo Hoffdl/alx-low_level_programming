9edb3c78550f77deed0225c67183b2d4ab1d8ea0
Ikegwuonu Henry <ikegwuonuhenry25@gmail.com>
2022-12-14 11:54:56 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - dont delete it!
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -17,7 +17,7 @@ int main(void)
 	{
 		putchar(i);
 	}
-
+	/* dont delete it! */
 	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
