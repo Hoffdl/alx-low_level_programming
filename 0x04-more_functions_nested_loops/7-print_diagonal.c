d0b18c6a94bcf539f7ef65a96e5bc856d9eecd96
Ikegwuonu Henry <ikegwuonuhenry25@gmail.com>
2022-12-16 20:29:10 -0800

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,23 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int i = 0,
+	int j;
 
-	for (i = 0; i < n; j++)
+	if (n > 0)
 	{
-		for (j = 0; j < i; j++)
+		for (; i < n; i++)
 		{
-			_putchar(' ');
-		}
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 		_putchar(92);
-		if (i < (n - 1))
-			_putchar('\n');
+		_putchar('\n');
+		}
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
