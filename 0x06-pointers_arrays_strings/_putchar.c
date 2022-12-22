6960c991b4061bf133d635d6bf326c7b785bbe59
Ikegwuonu Henry <ikegwuonuhenry25@gmail.com>
2022-12-21 18:12:07 -0800

    Task 0

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -3,9 +3,9 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: char to be printed
- * Return: 1 on success
+ * Return: 1
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1);
+	return (write(1, &c, 1));
 }
