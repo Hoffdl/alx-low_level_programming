a68d3e20481ce97ae8c8c6f77954e74878ca8c6a
Ikegwuonu Henry <ikegwuonuhenry25@gmail.com>
2023-01-04 09:53:35 -0800

    Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,5 +7,6 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
+void print_times_tables(int n);
 
 #endif
