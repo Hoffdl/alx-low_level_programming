f678ff04cc474075f5fc141a1161c0aa6e678ea3
Henry <ikegwwuonuhenry25@gmail.com>
2023-02-10 11:06:30 -0800

    File io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,23 +16,30 @@ size_t _strlen(char *str)
 
 /**
  * append_text_to_file - appends a text at the end of a file.
- * @filename: name of the fil
+ * @filename: name of the file to append
  * @text_content: NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len;
+	int fild = 0, chk = 0, i = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+
+	fild = open(filename, O_WRONLY | O_APPEND);
+	if (fild < 0)
 		return (-1);
-	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
-	close(fd);
-	if (len == -1)
-		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+
+		chk = write(fild, text_content, i);
+		if (chk < 0)
+			return (-1);
+	}
+	close(fild);
+	return (1);
 }
